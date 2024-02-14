85341753659d557afdc87fb2e8aaf922fd664332
OmarPinedaD <OmarPinedaD@users.noreply.github.com>
2024-02-14 16:50:47 -0600

    New DIV element text

--- src/app-index.ts
+++ src/app-index.ts
@@ -83,7 +83,7 @@ export class AppIndex extends LitElement {
     return html`
       <div>
         <main>
-          <div id="routerOutlet"></div>
+          <div id="routerOutlet">Developer</div>
         </main>
       </div>
     `;
